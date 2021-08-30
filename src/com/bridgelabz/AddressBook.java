a20ef8571e6c6e88e999a73d1c9e340312513a94
Gaurip10 <thegauriparhad@gmail.com>
2021-08-30 19:52:26 +0530

    Added: Ability to Edit

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Scanner;
 
-public class AddressBook implements AddressBookInterface{
+public class AddressBook implements AddressBookInterface {
     Scanner s = new Scanner(System.in);
     ArrayList<Person> list = new ArrayList<Person>();
 
@@ -40,7 +40,7 @@ public class AddressBook implements AddressBookInterface{
         } while (status);
     }
 
-    
+
     public void add() {
         Person person = new Person();
         Address address = new Address();
@@ -72,4 +72,45 @@ public class AddressBook implements AddressBookInterface{
 
     }
 
+    public void edit() {
+        System.out.println("Enter your First name:");
+        String fname = s.next();
+
+        Iterator<Person> iterator = list.listIterator();
+
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+
+            if (fname.equals(person.getFname())) {
+                Address address = person.getAddress();
+                System.out.println("Choose field you want to add:");
+                System.out
+                        .println("1.Last Name\t2.Phone Number\t3.City\t4.Zip\t5. State");
+                switch (s.nextInt()) {
+                    case 1:
+                        System.out.println("Re-Correct your lastname");
+                        person.setLname(s.next());
+                        break;
+                    case 2:
+                        System.out.println("Re-Correct your Phone Number");
+                        person.setPhonenumber(s.nextLong());
+                        break;
+                    case 3:
+                        System.out.println("Re-Correct your City");
+                        address.setCity(s.next());
+                        break;
+                    case 4:
+                        System.out.println("Re-Correct your Zip");
+                        address.setZip(s.nextLong());
+                        break;
+                    case 5:
+                        System.out.println("Re-Correct your State");
+                        address.setState(s.next());
+                        break;
+                }
+            }
+        }
+    }
+
 }
+            }
